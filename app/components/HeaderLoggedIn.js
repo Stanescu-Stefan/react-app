9976ad2d4f6e1652a367382c43d27cde504750aa
Stanescu Stefan <stanescu.irinel.stefan@gmail.com>
2022-03-01 21:45:30 +0200

    using useReducer/context+ code improvement

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-import ExampleContext from "../ExampleContext";
+import DispatchContext from "../DispatchContext";
 
 function HeaderLoggedIn() {
-  const { setLoggedIn } = useContext(ExampleContext);
+  const appDispatch = useContext(DispatchContext);
 
   function handleLogout() {
-    setLoggedIn(false);
+    appDispatch({ type: "logout" });
     localStorage.removeItem("reactappToken");
     localStorage.removeItem("reactappUsername");
     localStorage.removeItem("reactappAvatar");
--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState, useContext } from "react";
 import Axios from "axios";
-import ExampleContext from "../ExampleContext";
+import DispatchContext from "../DispatchContext";
 
 function HeaderLoggedOut() {
-  const { setLoggedIn } = useContext(ExampleContext);
+  const appDispatch = useContext(DispatchContext);
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
 
@@ -15,7 +15,7 @@ function HeaderLoggedOut() {
         localStorage.setItem("reactappToken", response.data.token);
         localStorage.setItem("reactappUsername", response.data.username);
         localStorage.setItem("reactappAvatar", response.data.avatar);
-        setLoggedIn(true);
+        appDispatch({ type: "login" });
       } else {
         console.log("Incorrect username / password");
       }
