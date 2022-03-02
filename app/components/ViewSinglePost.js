232b499d583023442f0b8652c07db81c0a97f32e
Stanescu Stefan <stanescu.irinel.stefan@gmail.com>
2022-03-02 21:00:20 +0200

    adding tooltip hover for actions

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -3,7 +3,8 @@ import Page from "./Page";
 import { useParams, Link } from "react-router-dom";
 import Axios from "axios";
 import LoadingDotsIcon from "./LoadingDotsIcon";
-import reactMarkdown from "react-markdown";
+import ReactMarkdown from "react-markdown";
+import ReactTooltip from "react-tooltip";
 
 function ViewSinglePost() {
   const { id } = useParams();
@@ -44,12 +45,14 @@ function ViewSinglePost() {
       <div className="d-flex justify-content-between">
         <h2>{post.title}</h2>
         <span className="pt-2">
-          <a href="#" className="text-primary mr-2" title="Edit">
+          <a href="#" data-tip="Edit" data-for="edit" className="text-primary mr-2">
             <i className="fas fa-edit"></i>
           </a>
-          <a className="delete-post-button text-danger" title="Delete">
+          <ReactTooltip id="edit" className="custom-tooltip" />{" "}
+          <a data-tip="Delete" data-for="delete" className="delete-post-button text-danger">
             <i className="fas fa-trash"></i>
           </a>
+          <ReactTooltip id="delete" className="custom-tooltip" />
         </span>
       </div>
 
