b1e2141f07c1bc1a92fbed6e3b5124c9e1bf0405
Stanescu Stefan <stanescu.irinel.stefan@gmail.com>
2022-03-02 20:20:25 +0200

    fixing Can't perform a React state update on an unmounted component issue

--- app/components/ProfilePosts.js
+++ app/components/ProfilePosts.js
@@ -9,9 +9,11 @@ function ProfilePosts(props) {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
+    const ourRequest = Axios.CancelToken.source();
+
     async function fetchPosts() {
       try {
-        const response = await Axios.get(`/profile/${username}/posts`);
+        const response = await Axios.get(`/profile/${username}/posts`, { cancelToken: ourRequest.token });
         setPosts(response.data);
         setIsLoading(false);
       } catch (e) {
@@ -19,6 +21,9 @@ function ProfilePosts(props) {
       }
     }
     fetchPosts();
+    return () => {
+      ourRequest.cancel();
+    };
   }, []);
 
   if (isLoading) return <LoadingDotsIcon />;
