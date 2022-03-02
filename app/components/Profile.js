e5215ba217e976ceb56f22adae7bdc4ea86e0c01
Stanescu Stefan <stanescu.irinel.stefan@gmail.com>
2022-03-02 18:31:16 +0200

    load posts by user id

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -3,6 +3,7 @@ import Page from "./Page";
 import { useParams } from "react-router-dom";
 import Axios from "axios";
 import StateContext from "../StateContext";
+import ProfilePosts from "./ProfilePosts";
 
 function Profile() {
   const { username } = useParams();
@@ -17,7 +18,7 @@ function Profile() {
     async function fetchData() {
       try {
         const response = await Axios.post(`/profile/${username}`, { token: appState.user.token });
-        console.log(response.data);
+        setProfileData(response.data);
       } catch (e) {
         console.log("There was a problem.");
       }
@@ -28,7 +29,7 @@ function Profile() {
   return (
     <Page title="Profile Screen">
       <h2>
-        <img className="avatar-small" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> brad
+        <img className="avatar-small" src={profileData.profileAvatar} /> {profileData.profileUsername}
         <button className="btn btn-primary btn-sm ml-2">
           Follow <i className="fas fa-user-plus"></i>
         </button>
@@ -36,30 +37,17 @@ function Profile() {
 
       <div className="profile-nav nav nav-tabs pt-2 mb-4">
         <a href="#" className="active nav-item nav-link">
-          Posts: 3
+          Posts: {profileData.counts.postCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Followers: 101
+          Followers: {profileData.counts.followerCount}
         </a>
         <a href="#" className="nav-item nav-link">
-          Following: 40
+          Following: {profileData.counts.followingCount}
         </a>
       </div>
 
-      <div className="list-group">
-        <a href="#" className="list-group-item list-group-item-action">
-          <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #1</strong>
-          <span className="text-muted small">on 2/10/2020 </span>
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #2</strong>
-          <span className="text-muted small">on 2/10/2020 </span>
-        </a>
-        <a href="#" className="list-group-item list-group-item-action">
-          <img className="avatar-tiny" src="https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128" /> <strong>Example Post #3</strong>
-          <span className="text-muted small">on 2/10/2020 </span>
-        </a>
-      </div>
+      <ProfilePosts />
     </Page>
   );
 }
