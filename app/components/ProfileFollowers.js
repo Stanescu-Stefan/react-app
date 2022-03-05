eda6c9b72eedcc0df5ea18d7f8d8dfe54ed8a3d5
Stanescu Stefan <stanescu.irinel.stefan@gmail.com>
2022-03-05 20:57:42 +0200

    implemented followers/following tabs

--- app/components/ProfileFollowers.js
+++ app/components/ProfileFollowers.js
@@ -1,9 +1,9 @@
 import Axios from "axios";
 import React, { useEffect, useState } from "react";
-import { useParams, Link, NavLink } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import LoadingDotsIcon from "./LoadingDotsIcon";
 
-function ProfilePosts(props) {
+function ProfileFollowers(props) {
   const { username } = useParams();
   const [isLoading, setIsLoading] = useState(true);
   const [posts, setPosts] = useState([]);
@@ -13,7 +13,7 @@ function ProfilePosts(props) {
 
     async function fetchPosts() {
       try {
-        const response = await Axios.get(`/profile/${username}/posts`, { cancelToken: ourRequest.token });
+        const response = await Axios.get(`/profile/${username}/followers`, { cancelToken: ourRequest.token });
         setPosts(response.data);
         setIsLoading(false);
       } catch (e) {
@@ -30,13 +30,10 @@ function ProfilePosts(props) {
 
   return (
     <div className="list-group">
-      {posts.map(post => {
-        const date = new Date(post.createdDate);
-        const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
-
+      {posts.map((follower, index) => {
         return (
-          <Link key={post._id} to={`/post/${post._id}`} className="list-group-item list-group-item-action">
-            <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong> <span className="text-muted small">on {dateFormatted} </span>
+          <Link key={index} to={`/profile/${follower.username}`} className="list-group-item list-group-item-action">
+            <img className="avatar-tiny" src={follower.avatar} /> {follower.username}
           </Link>
         );
       })}
@@ -44,4 +41,4 @@ function ProfilePosts(props) {
   );
 }
 
-export default ProfilePosts;
+export default ProfileFollowers;
