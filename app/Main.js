333c51bb6f86e82a7a55dbc7b1ad57a306a5dd00
Stanescu Stefan <stanescu.irinel.stefan@gmail.com>
2022-03-08 18:42:30 +0200

    check if user token is expired

--- app/Main.js
+++ app/Main.js
@@ -86,6 +86,28 @@ function Main() {
     }
   }, [state.loggedIn]);
 
+  // check if token has expired or not on first render
+
+  useEffect(() => {
+    if (state.loggedIn) {
+      // send axios request here
+      const ourRequest = Axios.CancelToken.source();
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/checkToken", { token: state.user.token }, { cancelToken: ourRequest.token });
+          if (!response.data) {
+            dispatch({ type: "logout" });
+            dispatch({ type: "flashMessage", value: "Your session has expired. Please log in again!" });
+          }
+        } catch (e) {
+          console.log("There was a problem or the request was cancelled.");
+        }
+      }
+      fetchResults();
+      return () => ourRequest.cancel();
+    }
+  }, []);
+
   return (
     <StateContext.Provider value={state}>
       <DispatchContext.Provider value={dispatch}>
